88b1b95bdca336f4fc2d8c36322c9b44a27def65
James Zheng <jzzheng@gmail.com>
2017-12-29 11:06:31 +0800

    Update sign.py

--- core/sign.py
+++ core/sign.py
@@ -6,6 +6,27 @@ import Crypto
 import os
 import pickle
 from binascii import hexlify, unhexlify
+import string
+
+ascd = dict(enumerate(string.ascii_letters + string.digits + '-+'))
+ascdr = dict(map(reversed, ascd.items()))
+
+def encode(n):
+    bn, bl = bin(n)[2:], n.bit_length()
+    nchars = (bl + 5) / 6
+    n_extra0 = nchars * 6 - bl
+    zeroed_n = '0'*n_extra0 + bn
+    l = [ascd[eval('0b'+zeroed_n[i*6:(i+1)*6])] for i in range(nchars)]
+    return ''.join(l)
+    
+def decode(s):
+    return eval('0b'+''.join([('00000'+bin(ascdr[i])[2:])[-6:] for i in 'a'*(171-len(s))+s]))
+
+encode_all = lambda l:'|'.join([encode(i) for i in l])
+decode_all = lambda s:[decode(i) for i in s.split('|')]
+
+PRIVATE_KEY_FORMAT = "(iCrypto.PublicKey.DSA\n_DSAobj\np0\n(dp2\nS'y'\np3\nL{0}L\nsS'p'\np4\nL{1}L\nsS'q'\np5\nL{2}L\nsS'g'\np6\nL{3}L\nsS'x'\np7\nL{4}L\nsb."
+PUBLIC_KEY_FORMAT  = "(iCrypto.PublicKey.DSA\n_DSAobj\np0\n(dp2\nS'y'\np3\nL{0}L\nsS'p'\np4\nL{1}L\nsS'q'\np5\nL{2}L\nsS'g'\np6\nL{3}L__________________\nsb."
 
 def sign(private_key, message, algorithm=DSA):
     if type(private_key) is str:
@@ -35,6 +56,7 @@ def create_key(algorithm=DSA):
     return key_tostring(private_key), key_tostring(public_key)
 
 def key_tostring(key):
+    print 'KEY: ' + str(pickle.dumps(key))
     return hexlify(pickle.dumps(key))
 
 def string_tokey(key_str):
@@ -60,4 +82,3 @@ print verify(public_key, message, signature)
 print 'private_key hex:', private_key
 print 'signature:', signature
 print 'public_key hex:', public_key
-
